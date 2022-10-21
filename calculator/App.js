0a8da4d15112299843069e2d8974411ff95acc09
Khang_OE <19521663@gm.uit.edu.vn>
2022-10-21 09:56:02 +0700

    fix local storage on simulator/device

--- calculator/App.js
+++ calculator/App.js
@@ -7,7 +7,8 @@ import {
   SafeAreaView,
   FlatList,
 } from "react-native";
-import AsyncStorage from "@react-native-community/async-storage";
+//import AsyncStorage from "@react-native-community/async-storage";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 const App = () => {
   const [text, setText] = useState("");
   const [prev, setPrev] = useState("");
